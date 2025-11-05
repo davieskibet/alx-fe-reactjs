4667518f9e4f6a4eb171c1aa477d84c32afb527e
Davies Kibet <davieskibet22@gmail.com>
2025-11-05 13:36:20 +0300

    Add WelcomeMessage component to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -6,6 +7,7 @@ import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
+      <WelcomeMessage />
       <Header />
       <MainContent />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
