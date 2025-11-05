00b59bf5497d61f5e248f876a2f61072c236be91
Davies Kibet <davieskibet22@gmail.com>
2025-11-05 10:08:57 +0300

    Add WelcomeMessage component and integrate JSX

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
