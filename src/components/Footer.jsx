27e12bd5bccc2d4fde428bbefc0870ad0bf24dac
Davies Kibet <davieskibet22@gmail.com>
2025-11-10 10:36:46 +0300

    Fresh React project with inline CSS components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>© 2025 My React App</p>
+    </footer>
+  );
+}
+
+export default Footer;
