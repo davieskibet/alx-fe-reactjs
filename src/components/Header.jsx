d9c03907a252ea345c4996d0423e61844c1a554e
Davies Kibet <davieskibet22@gmail.com>
2025-11-15 17:56:23 +0300

    Apply inline CSS styling to Header, UserProfile, MainContent, and Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
+    <h1>My Favorite Cities</h1>
+  </header>
+);
+
+export default Header;
