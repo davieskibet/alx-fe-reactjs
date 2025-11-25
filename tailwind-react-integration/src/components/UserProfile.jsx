f9b83a31b37208995fdd82e15049bf712c28953b
Davies Kibet <davieskibet22@gmail.com>
2025-11-25 21:43:52 +0300

    Task 3: Added hover effects, transitions, and animations to UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 transform transition duration-300">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 rounded-lg shadow-lg hover:shadow-xl transform transition-shadow duration-300">
       <img
         src="https://randomuser.me/api/portraits/men/32.jpg"
         alt="John Doe"
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 font-semibold text-center">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 font-semibold text-center hover:text-blue-500 transition-colors duration-300">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-base text-gray-600 text-center">
