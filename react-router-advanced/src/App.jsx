88b94ece21f8ccc8e3a25fa1d292dd34c9d966cc
Davies Kibet <davieskibet22@gmail.com>
2025-12-14 11:57:06 +0300

    Add dynamic route /blog/:id with BlogPost component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import BlogPost from "./components/BlogPost";
 
 function App() {
-  // Simulated authentication state
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
-        {/* Protected Profile route */}
         <Route
-          path="/profile"
+          path="/profile/*"
           element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        />
+        {/* Dynamic route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
