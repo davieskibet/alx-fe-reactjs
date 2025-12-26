e93d494a4340375f8716435702242502d8e2b76e
Davies Kibet <davieskibet22@gmail.com>
2025-12-26 12:02:30 +0300

    Fix Task 1: error message corrected for checker

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ function Search() {
       const data = await fetchUserData(username);
       setUser(data);
     } catch {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user"); // <-- exact string
     } finally {
       setLoading(false);
     }
